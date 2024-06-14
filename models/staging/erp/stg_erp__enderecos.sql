a3f3481684630a447f3723298612e19dc94e93ca
Luis Maeda <luishenriquemaeda@gmail.com>
2024-06-14 13:36:52 +0000

    stgs pedidos, paises, enderecos

--- models/staging/erp/stg_erp__enderecos.sql
+++ models/staging/erp/stg_erp__enderecos.sql
@@ -9,15 +9,15 @@ source as (
 enderecos as (
 
     select
-        addressid,
-        addressline1,
-        addressline2,
-        city,
-        stateprovinceid,
-        postalcode,
-        spatiallocation,
-        rowguid,
-        modifieddate
+        cast(addressid as int) as pk_endereco 
+        , cast(addressline1 as varchar) as endereco_linha_1
+        , cast(addressline2 as varchar) as endereco_linha_2
+        , cast(city as varchar) as cidade
+        , cast(stateprovinceid as int) as fk_estados_provincias
+        , cast(postalcode as varchar) as codigo_postal
+        , cast(spatiallocation as varchar) as localizacao_espacial
+        , cast(rowguid as varchar) as rowguid
+        , TO_CHAR(cast(MODIFIEDDATE as date)::DATE,'DD/MM/YYYY') as  data_modificacao
 
     from source
 
